374b7b7cead2e46fee044ffa218b2b2a344ef458
Aiventra Hub <aiventrahub@gmail.com>
2026-06-25 20:09:41 +0600

    Log Telegram object and user details
    Added console logs to display Telegram object and user information.

--- js/telegram.js
+++ js/telegram.js
@@ -3,6 +3,10 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
+console.log("Telegram Object:", tg);
+console.log("initData:", tg.initData);
+console.log("User:", tg.initDataUnsafe?.user);
+
 if (tg.initDataUnsafe?.user) {
     window.USER_ID = tg.initDataUnsafe.user.id;
     window.USERNAME = tg.initDataUnsafe.user.username || "";
