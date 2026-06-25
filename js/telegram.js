e59308446c852c404bdfae155bc986e4f0ad5dba
Aiventra Hub <aiventrahub@gmail.com>
2026-06-25 20:01:52 +0600

    Initialize Telegram Web App and log user info

--- js/telegram.js
+++ js/telegram.js
@@ -1,1 +1,12 @@
+const tg = window.Telegram.WebApp;
 
+tg.ready();
+tg.expand();
+
+if (tg.initDataUnsafe?.user) {
+    window.USER_ID = tg.initDataUnsafe.user.id;
+    window.USERNAME = tg.initDataUnsafe.user.username || "";
+
+    console.log("User ID:", window.USER_ID);
+    console.log("Username:", window.USERNAME);
+}
